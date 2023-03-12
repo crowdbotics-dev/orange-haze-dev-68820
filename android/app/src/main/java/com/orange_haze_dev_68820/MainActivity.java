39ea58301deaa0f862abe1270e01b8c27e952e6a
Crowdbotics <engineering@crowdbotics.com>
2023-03-12 00:08:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/orange_haze_dev_68820/MainActivity.java
@@ -0,0 +1,15 @@
+package com.orange_haze_dev_68820;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "orange_haze_dev_68820";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orange-haze-dev-68820",
+  url: "https://orange-haze-dev-68820.botics.co",
+  api: "https://orange-haze-dev-68820.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
